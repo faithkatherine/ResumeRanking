55ad4ef23916eb9f6393626b2888d5be20605f20
faith catherine <faithcathy12@gmail.com>
2021-05-08 14:04:39 +0300

    frontend

--- FrontendATS/src/app/app.module.ts
+++ FrontendATS/src/app/app.module.ts
@@ -3,21 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AccountComponent } from './account/account.component';
 import { JobsComponent } from './jobs/jobs.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AccountComponent,
-    JobsComponent
-  ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
+  declarations: [
+    AppComponent,
+    AccountComponent,
+    JobsComponent
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
