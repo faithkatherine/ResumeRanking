18b3ef8dfef9eba9416cd00ba38d955fda36b304
faith catherine <faithcathy12@gmail.com>
2021-05-08 11:53:50 +0300

    frontendapp

---
+++ FrontendATS/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { JobsComponent } from './jobs/jobs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AccountComponent,
+    JobsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
